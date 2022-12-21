970d13ae243e03fc40f2f0a9a50307565b6c6659
moriya <moriya.hadad@campus.technion.ac.il>
2022-12-21 13:46:47 +0200

    wet2

--- UF.h
+++ UF.h
@@ -2,56 +2,61 @@
 #define UF_H
 
 #include "hash.h"
+#include "AVL_tree.h"
 
 
 template<class T,class G>
-struct Node
+struct UF_Node
 {
-    Node* father;
+    UF_Node* father;
     T data;
     int height;
     G* group;
 
-    T& get_data_Node();
-    Node(T data): father(nullptr), data(data), height(1){}
+    explicit UF_Node(T data): father(nullptr), data(data), height(1){}
 
 };
 
 
-template<class T, class E, class G>
+template<class T, class G, class Cond>
 class UF
 {
-    Hash_table<E>* elements;
-    Node<T,G>* groups;
+    Hash_table<T, G>* elements;
+    AVL_Tree<G, Cond>* groups;
     int size;
 
 public:
     UF();
-    bool insert(E elem, T group);
-    void Union(T g1, T g2);
-    G* find(E elem) const;
+    bool insert(T elem, G group);
+    void Union(G g1, G g2);
+    G* find(int key);
 
 
 };
 
 
-template<class T, class E, class G>
-bool UF<T, E, Cond>::insert(E elem, T group)
+template<class T, class G, class Cond>
+bool UF<T, G, Cond>::insert(T elem, G group)
 {
-    Node<E> node1 = new Node<E>(elem);
+    UF_Node<T,G> node1 = new UF_Node<T,G>(elem);
     elements->add(elem->get_key(), node1);
     node1->group = group;
+    UF_Node<T, G> root = group->get_elements();
+    node1.father = root;
+
+    // elem->partial_spirit += team_spirit
+
 
     return true;
 }
 
-template<class T, class E, class G>
-void UF<T, E, Cond>::Union(T g1, T g2)
+template<class T, class G, class Cond>
+void UF<T, G, Cond>::Union(G g1, G g2)
 {
     if(g1 == g2)
         return;
-    T root1 = g1->get_root();
-    T root2 = g2->get_root();
+    UF_Node<T,G> root1 = g1->get_elements();
+    UF_Node<T,G> root2 = g2->get_elements();
     if (g1->get_size() > g2->get_size())
     {
         root2->make_father(root1);
@@ -64,27 +69,30 @@ void UF<T, E, Cond>::Union(T g1, T g2)
     }
 }
 
-template<class T, class E, class G>
-G* UF<T, E, Cond>::find(int key)
+// new functions- make_father and resize
+
+
+template<class T, class G, class Cond>
+G* UF<T, G, Cond>::find(int key)
 {
-  Pocket<T>* temp1 = this->elements.get(key);
-  if(temp1== nullptr)
-  {
-      return nullptr;
-  }
-  Node<T,G>* tempnode = temp1->node;
-  while(tempnode->father!= nullptr)
-  {
-      tempnode=tempnode->father;// hold top of the group
-  }
-  Node<T,G>* tempsqeezelines1=temp1->node;
-  Node<T,G>* tempsqeezelines2=temp1->node;
-  while(tempsqeezelines1->father!= nullptr)
-  {
-      tempsqeezelines2=tempsqeezelines1->father;
-      tempsqeezelines1->father = tempnode;
-      tempsqeezelines1=tempsqeezelines2;
-  }
+    Pocket<T, G>* temp1 = this->elements.get(key);
+    if(temp1 == nullptr)
+    {
+        return nullptr;
+    }
+    UF_Node<T,G>* tempnode = temp1->node;
+    while(tempnode->father!= nullptr)
+    {
+        tempnode=tempnode->father;// hold top of the group
+    }
+    UF_Node<T,G>* tempsqeezelines1=temp1->node;
+    UF_Node<T,G>* tempsqeezelines2=temp1->node;
+    while(tempsqeezelines1->father!= nullptr)
+    {
+        tempsqeezelines2=tempsqeezelines1->father;
+        tempsqeezelines1->father = tempnode;
+        tempsqeezelines1=tempsqeezelines2;
+    }
     return tempnode->group;
 }
 
